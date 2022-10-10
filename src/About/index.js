ff14e8515609129232e5a31e7aa4ae6ea064e298
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-10-10 13:34:18 -0300

    frontend updated

--- src/About/index.js
+++ src/About/index.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import './About.scss'
-import estudio from '../assets/shutterstock_537999616.jpg'
+import estudio from '../assets/estudio.png'
+import CHILEBLANCO from '../assets/CHILE BLANCO.png'
+import MUSICA from '../assets/MUSICAA.png'
+import REDE from '../assets/REDE.png'
+
 const About = () => {
   return (
     <div className='container-about' id='container-about'>
         <div id='about'></div>
-        <h1>Acerca de nosotros</h1>
-        <hr  color="white" />
-        <img className='img-phone' src={estudio}/>
+        
+        
+        
         <div className='container-p'>
          <div className='about'>
-            <h1>Acerca de nosotros</h1>
-            <p>
-            Somos un estudio con base en Santiago de Chile. Producimos música y sonido que emociona y permanece en la memoria. Trabajamos con las más importantes Agencias de Publicidad Productoras de Cine, TV, Internet y otros medios, tanto de Chile como el extranjero.
-            </p>
+            <h1>ACERCA DE NOSOTROS</h1>
+            {/* <p>
+            Somos un voces con base en Santiago de Chile. Producimos música y sonido que emociona y permanece en la memoria. Trabajamos con las más importantes Agencias de Publicidad Productoras de Cine, TV, Internet y otros medios, tanto de Chile como el extranjero.
+            </p> */}
          </div> 
           
             <div className='container-img'>
-            <img className='img-desktop' src={estudio}/>
-          </div>
+              <img className='img-desktop' src={estudio}/>
+            </div>
+
         </div>
+        <img className='img-phone' src={estudio}/>
+        <div className='flex-about'>
+            <img className='icon-about' src={CHILEBLANCO}/>
+            <hr className='hr'/>
+            <img className='icon-about' src={MUSICA}/>
+            <hr className='hr'/>
+            <img className='icon-about' src={REDE}/>
+        </div>
+        <p>Somos un estudio con base en Santiago de Chile.<br /> Producimos música y sonido que emociona y permanece en la memoria. <br /> Trabajamos con las más importantes Agencias de Publicidad Productoras de Cine, TV, internet y otros medios, tanto de Chile como el extranjero.</p>
     </div>
   )
 }
--- src/PulsoServices/index.js
+++ src/PulsoServices/index.js
@@ -3,27 +3,29 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Pagination, Navigation } from "swiper";
 import './PulsoServices.scss'
-import musica from '../assets/musicaOriginal.jpg'
+import musica from '../assets/musicaoriginal.jpg'
 import post from '../assets/postProduccion.jpg'
+import prod from '../assets/postP.jpg'
 import voces from '../assets/voces.jpg'
 import masterizacion from '../assets/masterizacion.jpg'
-import sincronizacion from '../assets/sincronizacion.jpg'
-
-
+import sincronizacion from '../assets/sincronización.jpg'
+import Modal from 'react-bootstrap/Modal';
+import voc from '../assets/vocesyloc.jpg'
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const PulsoServices = () => {
+    
   return (
     <div className='services' > 
             <div id='services'></div>
             <div className='header'>
-                <h1>Servicios</h1>
-                <hr  color="white" />
+                <h1>SERVICIOS</h1>
+                
             </div>
-            <Swiper
+            {/* <Swiper
             slidesPerView={3}
             spaceBetween={30}
             slidesPerGroup={3}
@@ -85,7 +87,7 @@ const PulsoServices = () => {
                     <h1 className='card-title'>Sincronización</h1>
                 </div>
             </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
             {/* <SwiperSlide>
                 <img className='slide-image' src={musica}/> 
                 <div className='card-content'>
@@ -120,16 +122,50 @@ const PulsoServices = () => {
         </div>
         </SwiperSlide> */}
 
-      </Swiper>
+      {/* </Swiper> */}
       <section className='cards-services'>
         <div className='card'>
             <div className='image-container'>
-            <img
-              src={musica}
-            />
                 <div className='card-content'>
-                    <h1 className='card-title'>Música original</h1>
+                    <h3 className='card-title'>Música original</h3>
+                        <p className='card-text'>Creamos música que emociona y que quieres volver a escuchar.</p>
                 </div>
+            <img src={musica}/>
+                
+            </div>
+        </div>
+        
+        
+
+
+   
+
+
+
+        <div className='card'>
+            <div className='image-container'>
+            <div className='card-content'>
+                <h3 className='card-title'>Post Producción de Sonido</h3>
+                        <p className='card-text'>Logramos un sonido único y memorable para todos tus proyectos.</p>
+                
+                </div>
+            <img
+              src={prod}
+            />
+               
+            </div>
+        </div>
+
+       
+
+        <div className='card'>
+            <div className='image-container'>
+                <div className='card-content'>
+                    <h3 id='voices' className='card-title'>Voces y Locucíón en Off</h3>
+                        <p className='card-text'>Tenemos las mejores voces en español e inglés.</p>
+                </div>
+            <img src={voc}/>
+                
             </div>
         </div>
 
@@ -139,29 +175,9 @@ const PulsoServices = () => {
               src={post}
             />
                 <div className='card-content'>
-                    <h1 className='card-title'>Post producción de sonido</h1>
-                </div>
-            </div>
-        </div>
-
-        <div className='card'>
-            <div className='image-container'>
-            <img
-              src={voces}
-            />
-                <div className='card-content'>
-                    <h1 className='card-title'>Voces y locución en off</h1>
-                </div>
-            </div>
-        </div>
-
-        <div className='card'>
-            <div className='image-container'>
-            <img
-              src={masterizacion}
-            />
-                <div className='card-content'>
-                    <h1 className='card-title'>Masterización</h1>
+                <h3 className='card-title'>Masterización</h3>
+                        <p className='card-text'>Entregamos un máster de alta fidelidad, que suena bien siempre y en todo lugar, con la mayor atención a los detalles de la producción.</p>
+                
                 </div>
             </div>
         </div>
@@ -172,7 +188,9 @@ const PulsoServices = () => {
               src={sincronizacion}
             />
                 <div className='card-content'>
-                    <h1 className='card-title'>Sincronización</h1>
+                <h3 className='card-title'>Sincronización</h3>
+                        <p className='card-text'>Unimos marcas con la mejor música del mundo.</p>
+                
                 </div>
             </div>
         </div>
